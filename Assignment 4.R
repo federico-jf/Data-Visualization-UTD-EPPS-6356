128d1273598747aee8f3015de5b9b304d5c88000
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2020-10-06 08:24:16 -0500

    Update Assignment 4.R

--- Assignment 4.R
+++ Assignment 4.R
@@ -23,3 +23,18 @@ barplot<-ggplot(data=mydata, aes(x=Difference, y=Percentage)) +
   theme_minimal() + 
   theme(text=element_text(size=13,  family="serif"))
 barplot  
+
+
+# reading in the data
+mydata2 <- read.delim("/Users/feder/Desktop/top_tweeters.txt")
+head(mydata2)
+
+barplot2<-ggplot(data=mydata2, aes(x=number_of_followers, y=from_user)) +
+  geom_bar(stat="identity", width=0.9, fill="lightgreen")+
+  geom_text(aes(label=number_of_followers), vjust=-0.3, size=3.5)+
+  labs(title="Top 10 tweeters using #AlevelResults by location", 
+       x="Number of followers", y = "Tweet users")+
+  theme_minimal() + 
+  theme(text=element_text(size=13,family="NimbusRom"))+
+  facet_wrap(~location, ncol=2)
+barplot2 
