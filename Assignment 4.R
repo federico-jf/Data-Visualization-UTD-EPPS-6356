5bee73f75c307b82e71d9769dca7363ddcc8b2cd
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2020-10-06 07:51:35 -0500

    Update Assignment 4.R

--- Assignment 4.R
+++ Assignment 4.R
@@ -3,6 +3,10 @@
 #Federico Ferrero
 #Data Visualization
 
+
+# Clear environment 
+rm(list=ls())
+
 # reading in the data
 mydata <- read.delim("/Users/feder/Desktop/Alevelresults.txt")
 
@@ -12,7 +16,10 @@ head(mydata)
 
 # Basic barplot
 barplot<-ggplot(data=mydata, aes(x=Difference, y=Percentage)) +
-  geom_bar(stat="identity", width=0.9, fill="steelblue")+
+  geom_bar(stat="identity", width=0.9, fill="lightblue")+
   geom_text(aes(label=Percentage), vjust=-0.3, size=3.5)+
-  theme_minimal()
-barplot
+  labs(title="More than a third of A-level results in England downgraded", 
+       x="Difference with teachers' predictions", y = "Percentage")+
+  theme_minimal() + 
+  theme(text=element_text(size=13,  family="serif"))
+barplot  
