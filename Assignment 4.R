ba0bcae6d71d81f2d851f3c08f7cd884d8496655
Federico Ferrero <53381800+federico-jf@users.noreply.github.com>
2020-10-06 11:20:06 -0500

    Update Assignment 4.R

--- Assignment 4.R
+++ Assignment 4.R
@@ -18,12 +18,11 @@ head(mydata)
 barplot<-ggplot(data=mydata, aes(x=Difference, y=Percentage)) +
   geom_bar(stat="identity", width=0.9, fill="lightblue")+
   geom_text(aes(label=Percentage), vjust=-0.3, size=3.5)+
-  labs(title="More than a third of A-level results in England downgraded", 
+  labs(title="More than a third of A-level results in England downgraded", caption= "Source: BBC", 
        x="Difference with teachers' predictions", y = "Percentage")+
   theme_minimal() + 
   theme(text=element_text(size=13,  family="serif"))
 barplot  
-
 
 # Barplot faceted barplot: Top 10 tweeters using #AlevelResults by location
 #reading in the data
